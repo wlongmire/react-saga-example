68b76e9e36351f20baed69b97cccdfde20a09af6
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-07-17 16:22:08 +0300

    (Chore) Updates the index file in the Users folder to be able to serve as a public API for interfacing as well

---
+++ src/users/index.ts
@@ -0,0 +1,43 @@
+import {ActionType, Action} from './actions';
+// import {Patient, Patients} Update this part once we get to make the necessary exports in the respective folders.
+
+import {ReducerState, Patient} from './models';
+import {getAllPatients} from './selectors';
+// import {root} from './sagas';
+import {reducer} from './reducer';
+
+/**
+ * As per the layout spec in the Cases section, this is a public API that we can exploit through which our modules can
+ * interface with each other.
+ * 
+ * It exposes all the elements,modules, components within this particular users module that we will continue 
+ * updating to be able to achieve the necessary functionality
+ */
+
+
+export const Model = {
+    ReducerState,
+    Patient
+}
+
+export const Actions = {
+    ActionType,
+    Action
+}
+
+export const Components = {
+    Patient,
+    // PAtients,Doctors and all other users will be here
+}
+
+export const Selectors = {
+    getAllPatients
+}
+
+// export const Sagas = {
+//     root
+// }
+
+export const Reducers = {
+    root: reducer
+}

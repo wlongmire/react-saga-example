5cd07ad705096e12d98a5dc8f5ad37102a3d55fb
Toby Jackson <toby@gethewson.com>
2017-07-17 08:32:45 -0400

    api module

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,10 +1,5 @@
 import { VisitService } from './visit';
 
 export const api = {
-<<<<<<< HEAD
     visits: VisitService
-=======
-    cases: new Api('cases'),
-    users: new Api('users'),
->>>>>>> api updates
 };

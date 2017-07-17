5610bf3c03c98a888fd58d58f74ebc7208e27a47
Toby Jackson <toby@gethewson.com>
2017-07-17 08:31:10 -0400

    login component

---
+++ src/login/index.ts
@@ -0,0 +1,11 @@
+import { ActionType, Action } from './actions';
+import { Login } from './components';
+
+export const Actions = {
+    ActionType, 
+    Action
+};
+
+export const Components = {
+    Login
+};
--- src/login/sagas.ts
+++ src/login/sagas.ts
@@ -1,4 +1,5 @@
 import * as Common from '../common';
+<<<<<<< HEAD
 import { all, fork, put, takeEvery } from 'redux-saga/effects';
 import * as Actions from './actions';
 
@@ -6,6 +7,16 @@ function* login(action: Common.ActionResult<{}>) {
     try {
         // const result = yield call(api.users.get, 1);
         yield(put(Actions.Action.loginSuccess({})));
+=======
+import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
+import * as Actions from './actions';
+import { api } from '../services/api';
+
+function* login(action: Common.ActionResult<{}>) {
+    try {
+        const result = yield call(api.users.get, 1);
+        yield(put(Actions.Action.loginSuccess(result)));
+>>>>>>> login component
     } catch (e) {
         yield(put(Actions.Action.loginFail(e)));
     }

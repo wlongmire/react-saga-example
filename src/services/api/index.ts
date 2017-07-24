7524c5556a7af4a8807176c3adc1f599cb029429
Toby Jackson <toby@gethewson.com>
2017-07-23 23:50:59 -0400

    api updates

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,10 @@
 import { VisitService } from './visit';
 
 export const api = {
+<<<<<<< HEAD
     visits: VisitService
+=======
+    cases: new Api('cases'),
+    users: new Api('users'),
+>>>>>>> api updates
 };

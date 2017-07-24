4c2d1c096764fd32980b11f1b6932543e494e994
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-07-24 20:06:09 +0300

    (Fix) Resolves issue with popover showing at the bottom

--- src/visits/components/Visit.js
+++ src/visits/components/Visit.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   '#visit-drawer': {
-    'position': 'fixed',
+    'position': 'absolute',
     'right': [{ 'unit': 'px', 'value': 0 }],
     'top': [{ 'unit': 'px', 'value': 0 }],
     'width': [{ 'unit': '%H', 'value': 0.3 }],

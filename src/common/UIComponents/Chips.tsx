7064e24bfabfc2ede6c4e52298de015c2e68672b
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-08-28 10:15:50 +0300

    (WIP) Add tests section files

---
+++ src/common/UIComponents/Chips.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Chip from 'material-ui/Chip';
+
+const styles = {
+  chip: {
+    margin: 4,
+  }
+};
+
+interface ChipCollectionProps {
+    items : Array<string>;
+}
+
+export class ChipCollection extends React.Component<ChipCollectionProps, {}>{
+    constructor(props:ChipCollectionProps){
+        super(props)
+     
+    }
+    handleRequestDelete = () => {
+        alert('Delete')
+    }
+    render(){
+            return(
+            <div>
+            {
+                this.props.items.map((i:any, index:number)=>{
+                    return(
+                            <Chip
+                                key={index}
+                                onRequestDelete={this.handleRequestDelete}
+                                style={styles.chip}
+                            >
+                            {i}
+                            </Chip>
+                    )
+                })
+            }
+            </div>
+            )
+    }
+}
---
+++ src/common/UIComponents/TableInputs.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'add': {
+    'color': '#f84445',
+    'fontSize': [{ 'unit': 'string', 'value': 'x-large' }],
+    'cursor': 'pointer'
+  }
+});

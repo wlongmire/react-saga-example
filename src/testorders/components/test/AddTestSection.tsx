73465f498befffa58a42fbd48075425573a3e0b2
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-08-28 15:54:56 +0300

    (WIP) Comment out tests section

--- src/testorders/components/test/AddTestSection.tsx
+++ src/testorders/components/test/AddTestSection.tsx
@@ -1,19 +1,22 @@
-import * as React from 'react';
-import Checkbox from 'material-ui/Checkbox';
-import DatePicker from 'material-ui/DatePicker';
-import RaisedButton from 'material-ui/RaisedButton';
-import {
-    TableInputs, 
-    TableTemplate,
-    DropDownTemplate, 
-    TextInputTemplate,
-    ChipCollection
-} from '../../../common/UIComponents';
+// import * as React from 'react';
+// import Checkbox from 'material-ui/Checkbox';
+// import DatePicker from 'material-ui/DatePicker';
+// import RaisedButton from 'material-ui/RaisedButton';
+// import {
+//     TableInputs, 
+//     TableTemplate,
+//     DropDownTemplate, 
+//     TextInputTemplate,
+//     ChipCollection
+// } from '../../../common/UIComponents';
 
-export class AddTestSection extends React.Component<{}, {}>{
-    render(){
-        return(
-            
-        )
-    }
-}
+// export class AddTestSection extends React.Component<{}, {}>{
+//     render(){
+//         return(
+//             <div>
+//                 Tests here
+
+//             </div>
+//         )
+//     }
+// }

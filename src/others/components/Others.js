adabefdcfed1e1e16ec80048ca711c290c64f13a
kimani-ndegwa <kimani.ndegwa@andela.com>
2017-09-22 20:36:29 +0300

    (Chore) Removes the react native files

--- src/others/components/Others.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  'others-component': {
-    'marginTop': [{ 'unit': 'em', 'value': 1 }],
-    'position': 'relative',
-    'left': [{ 'unit': 'em', 'value': 2 }]
-  }
-});
